de1fc9db8595d9ae6cf7366a98d39cc11bc6882f
woorimj <forestwr06@gmail.com>
2023-05-18 15:40:24 +0900

    add style

--- triplog/diary/urls.py
+++ triplog/diary/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from diary import views
 
 urlpatterns = [
-    path('', views.home, name='diaryHome')
+    path('', views.post, name='postHome'),
+    path('home/', views.home, name='diaryHome'),
 ]
--- triplog/diary/views.py
+++ triplog/diary/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def home(request): 
 	return render(request, "diaryHome.html")
+
+def post(request): 
+	return render(request, "post.html")
+
+
