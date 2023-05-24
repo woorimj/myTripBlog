29a0bc9696cfb749a8c20ddc843268a59458ea53
woorimj <forestwr06@gmail.com>
2023-05-24 16:26:49 +0900

    diaryHome edit

--- triplog/diary/views.py
+++ triplog/diary/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def home(request): 
-	return render(request, "diaryHome.html")
+	return render(request, "diaryIndex.html")
 
 def post(request): 
 	return render(request, "post.html")
